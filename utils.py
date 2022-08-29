4e3e54af4071d4f08ce5534fae891e79da0f563c
roscibely <rosanacibely@gmail.com>
2022-08-29 18:09:52 -0300

    feat: added generate_synthetic_data

---
+++ utils.py
@@ -0,0 +1,43 @@
+import pendulum 
+from pendulum import DateTime
+from pendulum import duration
+import pandas as pd
+import numpy as np
+
+class Utils():
+
+    def __init__(self):
+        pass
+
+
+    def generate_synthetic_data(duration_cycle, start_day, year, start_month_index=1, number_of_cycle = 5, period_duration = 30):
+        """ 
+        Args: 
+            duration_cycle (int): duration of the cycle in days
+            start_day (int): day of the first cycle
+            year (int): year of the first cycle
+            start_month_index (int): month of the first cycle
+            number_of_cycle (int): number of cycles
+            period_duration (int): duration of the period between cycles in days
+
+        Return: 
+            df (pd.DataFrame): dataframe with the synthetic data
+        """
+        data_frame = pd.DataFrame(columns=['M', 'Day', 'Year', 'Duration'])
+
+        start_time = DateTime(year, start_month_index, start_day, 1, 0, 0)
+        end_time = start_time+duration(days=duration_cycle) 
+
+
+        for _ in range(0,number_of_cycle+1): 
+
+            data_frame =pd.concat([data_frame, pd.DataFrame(np.array([[start_time.month, start_time.day, start_time.year, 'Starts']]),
+            columns=['M', 'Day', 'Year', 'Duration'])],  ignore_index = True, axis=0)
+
+            data_frame =pd.concat([data_frame, pd.DataFrame(np.array([[end_time.month, end_time.day, end_time.year, 'Ends']]),
+            columns=['M', 'Day', 'Year', 'Duration'])],  ignore_index = True, axis=0)
+
+            start_time =start_time+duration(days=period_duration)
+            end_time = start_time+duration(days=duration_cycle) 
+
+        return data_frame

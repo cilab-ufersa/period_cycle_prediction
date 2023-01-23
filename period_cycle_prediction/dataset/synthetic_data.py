d3054152003bcbd6b0b6f58a63c338b7cb200116
roscibely <rosanacibely@gmail.com>
2023-01-23 15:39:51 -0300

    feat: added synthetic data

--- period_cycle_prediction/dataset/synthetic_data.py
+++ period_cycle_prediction/dataset/synthetic_data.py
@@ -1,6 +1,7 @@
+import sys
+sys.path.append("../period_cycle_prediction/")
 from period_cycle_prediction.utils import generate_synthetic_data
 
-df = generate_synthetic_data(duration_cycle=5, start_day=25, year=2021, start_month_index=1, number_of_cycle = 5, period_duration = 30)
+dataset = generate_synthetic_data(duration_cycle=5, start_day=25, year=2021, start_month_index=1, number_of_cycle = 24, period_duration = 30)
 
-print(df.head())
-
+dataset.to_csv('period_cycle_prediction\dataset\synthetic_data.csv', index=False)
